b26ca4dbfad65561316380b1cd39d4504e9658c5
BornioAsley <nuquibg.647.stud@cdd.edu.ph>
2024-07-30 16:11:55 +0800

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 
 Route::resource('user_tasks', UserTaskController::class);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
