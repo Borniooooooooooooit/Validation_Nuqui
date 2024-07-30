d1593f0223080c20eeeb2ffb1a4501848f339d9e
BornioAsley <nuquibg.647.stud@cdd.edu.ph>
2024-07-30 14:25:01 +0800

    Validation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserTaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('user_tasks', UserTaskController::class);
+
